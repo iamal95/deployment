a6b00a1e6a57510bacc5eace211989f317b6af8a
Amal AlThaqafi <93076368+iamal95@users.noreply.github.com>
2021-12-21 11:44:15 +0300

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,3 +1,229 @@
 import streamlit as st
+import pickle
+import numpy as np
+import pandas as pd
 
-st.header("hi")
+    
+def load_model():
+    with open('saved_steps.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+xgboost = data["model"]
+columns = ['Current_Loan_Amount', 'Annual_Income', 'Monthly_Debt',
+       'Current_Credit_Balance', 'Maximum_Open_Credit', 'Tax_Liens',
+       'Home_Ownership_Home_Mortgage', 'Home_Ownership_Own_Home',
+       'Home_Ownership_Rent', 'Purpose_Buy_House', 'Purpose_Buy_a_Car',
+       'Purpose_Debt_Consolidation', 'Purpose_Educational_Expenses',
+       'Purpose_Home_Improvements', 'Purpose_Medical_Bills', 'Purpose_Other',
+       'Purpose_Take_a_Trip', 'Purpose_major_purchase', 'Purpose_moving',
+       'Purpose_renewable_energy', 'Purpose_small_business',
+       'Purpose_vacation', 'Purpose_wedding', 'Loan_Status_Fully_Paid',
+       'Years_in_current_job_10+_years', 'Years_in_current_job_2_years',
+       'Years_in_current_job_3_years', 'Years_in_current_job_4_years',
+       'Years_in_current_job_5_years', 'Years_in_current_job_6_years',
+       'Years_in_current_job_7_years', 'Years_in_current_job_8_years',
+       'Years_in_current_job_9_years', 'Years_in_current_job_less_1_year']
+
+
+
+def show_predict_page():
+    st.title("Loan Predict")
+
+    st.write("""### We need some information for prediction""")
+
+    
+    number1 = st.number_input('Insert Your Current Loan Amount')
+    Current_Loan_Amount = st.write('Your Current Loan Amount is ', number1)
+    
+    number2 = st.number_input('Insert Your Annual Income')
+    Annual_Income = st.write('Your Annual Income is ', number2)
+    
+    number3 = st.number_input('Insert Your Monthly Debt')
+    Monthly_Debt = st.write('Your Monthly Debt is ', number3)
+    
+    number4 = st.number_input('Insert Your Current Credit Balance')
+    Current_Credit_Balance = st.write('Your Current Credit Balance is ', number4)
+    
+    number5 = st.number_input('Insert Your Maximum Open Credit')
+    Maximum_Open_Credit = st.write('Your Maximum Open Credit is ', number5)
+  
+    
+  
+    
+  
+    option1 = st.slider('Your Tax Liens:', 0, 15, 1)
+    Tax_Liens = st.write('You selected:', option1)
+
+
+
+    option2 = st.selectbox(
+     'Select Your Home Ownership', (
+         'Home Mortgage',
+         'Own Home',
+         'Rent',
+         'Have Mortgage'
+         ))
+    Home_Ownership = st.write('You selected:', option2)
+    
+    
+    option3 = st.selectbox(
+     'Select Your Purpose', (
+         'Debt Consolidation',
+         'Buy House',
+         'Home Improvements',
+         'Other',
+         'Business Loan', 
+         'small business', 
+         'Take a Trip', 
+         'Buy a Car',
+         'Medical Bills', 
+         'major purchase', 
+         'moving', 
+         'wedding',
+         'Educational Expenses', 
+         'renewable energy', 
+         'vacation'
+         ))
+    Purpose = st.write('You selected:', option3)
+    
+    
+    option4 = st.selectbox(
+     'Select Your Loan Status', (
+         'Fully Paid', 
+         'Charged Off'
+         ))
+    Loan_Status = st.write('You selected:', option4)
+    
+    
+    option5 = st.selectbox(
+     'Select How Many Years in Current Job', (
+         '10 years or more', 
+         '9 years', 
+         '8 years', 
+         '7 years', 
+         '6 years',
+         '5 years', 
+         '4 years', 
+         '3 years', 
+         '2 years', 
+         '1 year', 
+         'less than year'
+         ))
+    Years_in_current_job = st.write('You selected:', option5)
+
+
+    ok = st.button("Predict")
+
+    if Tax_Liens == "0.":
+        Tax_Liens = 1
+    elif Tax_Liens == "1.":
+        Tax_Liens = 1
+    elif Tax_Liens == "2.":
+        Tax_Liens = 1
+    elif Tax_Liens == "3.":
+        Tax_Liens = 1
+    elif Tax_Liens == "4.":
+        Tax_Liens = 1
+    elif Tax_Liens == "5.":
+        Tax_Liens = 1
+    elif Tax_Liens == "6.":
+        Tax_Liens = 1
+    elif Tax_Liens == "9.":
+        Tax_Liens = 1
+    elif Tax_Liens == "10.":
+        Tax_Liens = 1
+    elif Tax_Liens == "11.":
+        Tax_Liens = 1
+    elif Tax_Liens == "15.":
+        Tax_Liens = 1
+    else:
+        Tax_Liens = 0
+
+
+
+    if Home_Ownership == "Own Home":
+        Home_Ownership = 1
+    else:
+        Home_Ownership = 0
+
+
+    if Purpose == 'Debt Consolidation':
+        Purpose = 1
+    elif Purpose == 'Buy House':
+        Purpose = 1
+    elif Purpose == 'Home Improvements':
+        Purpose = 1
+    elif Purpose == 'Other':
+        Purpose = 1
+    elif Purpose == 'Business Loan':
+        Purpose = 1
+    elif Purpose == 'small business':
+        Purpose = 1
+    elif Purpose == 'Take a Trip':
+        Purpose = 1
+    elif Purpose == 'Buy a Car':
+        Purpose = 1
+    elif Purpose == 'Medical Bills':
+        Purpose = 1
+    elif Purpose == 'major purchase':
+        Purpose = 1
+    elif Purpose == 'moving':
+        Purpose = 1
+    elif Purpose == 'wedding':
+        Purpose = 1
+    elif Purpose == 'Educational Expenses':
+        Purpose = 1
+    elif Purpose == 'renewable energy':
+        Purpose = 1
+    elif Purpose == 'vacation':
+        Purpose = 1
+    else:
+        Purpose = 0
+
+
+    if Loan_Status == 'Fully Paid':
+        Loan_Status = 1
+    else:
+        Loan_Status = 0
+         
+      
+    
+    if Years_in_current_job ==  '10 years or more':
+        Years_in_current_job = 1
+    elif Years_in_current_job == '9 years':
+        Years_in_current_job = 1
+    elif Years_in_current_job == '8 years':
+        Years_in_current_job = 1
+    elif Years_in_current_job == '7 years':
+        Years_in_current_job = 1
+    elif Years_in_current_job == '6 years':
+        Years_in_current_job = 1
+    elif Years_in_current_job == '5 years':
+        Years_in_current_job = 1
+    else:
+        Years_in_current_job = 0
+
+    
+
+    if ok:
+        X = np.array([[ Current_Loan_Amount, Annual_Income, Monthly_Debt, 
+                       Current_Credit_Balance, Maximum_Open_Credit, 
+                       Tax_Liens, Home_Ownership, Purpose,
+                       Loan_Status, Years_in_current_job]])
+        
+        X = pd.DataFrame(columns=columns)
+
+        prediction = xgboost.predict(X)
+
+        if prediction == 1:
+            prediction = "Short Term Loan"
+        else:
+            prediction = "Long Term Loan"
+            
+        st.subheader(f"Your Loan Term is: \n {prediction}")
+
+
+show_predict_page()
